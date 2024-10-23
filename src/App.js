eb786835325f3478be0adf78a69b508f6ffafd70
Melih Erpek <melih.erpek1@ogr.sakarya.edu.tr>
2024-10-23 14:44:30 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Router from "./Router/Router";
+import React, { useState } from "react";
+import { AuthContextProvider } from "./Context/AuthContext";
 
 function App() {
+  // async function register(e) {
+  //   e.preventDefault();
+  //   let response = await axios.post("http://localhost:5000/find", {
+  //     mail,
+  //     username,
+  //     password,
+  //   });
+  // }
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthContextProvider value={{ userData, setUserData }}>
+        <Header />
+        <Router />
+        <Footer />
+      </AuthContextProvider>
     </div>
   );
 }
